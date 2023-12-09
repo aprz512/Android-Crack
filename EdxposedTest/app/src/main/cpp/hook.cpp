4acdfeea7cc1494beecb389abe74dbb56a1798e3
aprz512 <aprz512@163.com>
2023-12-09 23:32:34 +0800

    add exposed hook so demo with shadowhook

---
+++ EdxposedTest/app/src/main/cpp/hook.cpp
@@ -0,0 +1,5 @@
+//
+// Created by root on 12/9/23.
+//
+
+#include "hook.h"
---
+++ EdxposedTest/app/src/main/cpp/hook.h
@@ -0,0 +1,14 @@
+//
+// Created by root on 12/9/23.
+//
+
+#ifndef EDXPOSEDTEST_HOOK_H
+#define EDXPOSEDTEST_HOOK_H
+
+
+class hook {
+
+};
+
+
+#endif //EDXPOSEDTEST_HOOK_H
